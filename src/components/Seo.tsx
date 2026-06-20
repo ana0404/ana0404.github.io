13f860b3af360bd58bf7c475db760a0969e4464f
ana0404 <anamika.d41290@gmail.com>
2026-06-20 05:13:05 -0700

    Revert "add google console tags"
    This reverts commit bb7b47135c29a254df1132b4aacb1b34e2c3326b.

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -9,7 +9,6 @@ interface SeoProps {
   author?: string;
   twitterHandle?: string;
   canonical?: string;
-  googleVerification?: string;
 }
 
 const defaultMetadata = {
@@ -44,7 +43,6 @@ export default function Seo({
     author: author || defaultMetadata.author,
     twitterHandle: twitterHandle || defaultMetadata.twitterHandle,
     canonical: canonical || defaultMetadata.canonical,
-    googleVerification,
   };
 
   return (
@@ -70,9 +68,6 @@ export default function Seo({
       <meta name="twitter:title" content={metadata.title} />
       <meta name="twitter:description" content={metadata.description} />
       <meta name="twitter:image" content={metadata.image} />
-      {metadata.googleVerification ? (
-        <meta name="google-site-verification" content={metadata.googleVerification} />
-      ) : null}
 
       <script type="application/ld+json">
         {`{
